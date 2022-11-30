69d4f83ed2d4d86ef2e9fe40f8d2b2726fad7973
grubeben <gr.game.98@gmail.com>
2022-11-30 21:37:01 +0100

    done

--- a7/plot.py
+++ a7/plot.py
@@ -6,99 +6,97 @@ from subprocess import Popen, PIPE
 import subprocess
 import seaborn as sns
 
+
+
 # import sys
 
 # files
-filepath1 = "data/ex1rtx.txt"
-data_ex1 = np.genfromtxt(filepath1, dtype=float, delimiter=' ')
-filepath2 = "data/ex1k40.txt"
-data_ex2 = np.genfromtxt(filepath2, dtype=float, delimiter=' ')
+filepath1 = "data/ref_rtx.txt"
+data_1 = np.genfromtxt(filepath1, dtype=float, delimiter=' ')
+filepath2 = "data/pipe_rtx.txt"
+data_2 = np.genfromtxt(filepath2, dtype=float, delimiter=' ')
 
-filepath3 = "data/ex2rtx.txt"
-data_3= np.genfromtxt(filepath3, dtype=float, delimiter=' ')
-filepath4 = "data/ex2k40.txt"
-data_4= np.genfromtxt(filepath4, dtype=float, delimiter=' ')
-filepath6 = "data/ex2cg.txt"
-data_6= np.genfromtxt(filepath6, dtype=float, delimiter=' ')
+# filepath3 = "data/ex2rtx.txt"
+# data_3= np.genfromtxt(filepath3, dtype=float, delimiter=' ')
+# filepath4 = "data/ex2k40.txt"
+# data_4= np.genfromtxt(filepath4, dtype=float, delimiter=' ')
+# filepath6 = "data/ex2cg.txt"
+# data_6= np.genfromtxt(filepath6, dtype=float, delimiter=' ')
 
 
-filepath5 = "data/ex2bonus.txt"
-data_ex5 = np.genfromtxt(filepath5, dtype=float, delimiter=' ')
+# filepath5 = "data/ex2bonus.txt"
+# data_ex5 = np.genfromtxt(filepath5, dtype=float, delimiter=' ')
 
 
 
 # plot - ex2rtx
 N = []
-t_gpu= []
-t_cpu= []
+t_ref= []
+t_pipe= []
 t_cg=[]
 
-for i in range(len(data_3)):
-    N.append(data_3
+for i in range(len(data_1)):
+    N.append(data_1
              [i][0])
-    t_gpu.append(data_3
+    t_ref.append(data_1
              [i][1])
-    t_cpu.append(data_3
-             [i][2])
-    t_cg.append(data_6
+    t_pipe.append(data_2
              [i][1])
 
 
 plt.figure(figsize=(10, 5))
-plt.plot(N, t_gpu,
-         label="assembly via gpu", color='y', linestyle='dotted', linewidth=10)
-plt.plot(N, t_cpu,
-         label="assembly via cpu", color='r')
-plt.plot(N, t_cg,
-         label="cg kernel", color='b')
+plt.plot(N, t_ref,
+         label="cg reference implementation", color='y')
+plt.plot(N, t_pipe,
+         label="pipelined cg implementation", color='r')
 #plt.plot(k, data , label = "time_custom")
 plt.xscale('log', base=10)
 plt.yscale('log', base=10)
-plt.xlabel("grid size N (=points in one direction)")
+plt.xlabel("number of unknowns")
 plt.ylabel("computation time[ms]")
 plt.legend()
 plt.title(
-    "Performance of GPU- vs CPU-powered matrix assembly on RTX3060 compared to GPU-CG on RTX3060")
+    "Performance of GPU CG-implementations on RTX3060")
 plt.grid()
-plt.savefig("plots/ex2rtx.jpg", bbox_inches='tight')
+plt.savefig("plots/rtx.jpg", bbox_inches='tight')
 
 
 
 
-# plot - ex2k40
-N = []
-t_gpu= []
-t_cpu= []
-t_cg=[]
+# # plot - ex2k40
+# N = []
+# t_ref= []
+# t_pipe= []
+# t_cg=[]
 
-for i in range(len(data_4)):
-    N.append(data_4
-             [i][0])
-    t_gpu.append(data_4
-             [i][1])
-    t_cpu.append(data_4
-             [i][2])
-    t_cg.append(data_6
-             [i][1])
+# for i in range(len(data_4)):
+#     N.append(data_4
+#              [i][0])
+#     t_ref.append(data_4
+#              [i][1])
+#     t_pipe.append(data_4
+#              [i][2])
+#     t_cg.append(data_6
+#              [i][1])
 
 
-plt.figure(figsize=(10, 5))
-plt.plot(N, t_gpu,
-         label="assembly via gpu", color='y', linestyle='dotted', linewidth=10)
-plt.plot(N, t_cpu,
-         label="assembly via cpu", color='r')
-plt.plot(N, t_cg,
-         label="cg kernel", color='b')
-#plt.plot(k, data , label = "time_custom")
-plt.xscale('log', base=10)
-plt.yscale('log', base=10)
-plt.xlabel("grid size N (=points in one direction)")
-plt.ylabel("computation time[ms]")
-plt.legend()
-plt.title(
-    "Performance of GPU- vs CPU-powered matrix assembly on K40 compared to GPU-CG on RTX3060")
-plt.grid()
-plt.savefig("plots/ex2k40.jpg", bbox_inches='tight')
+# plt.figure(figsize=(10, 5))
+# plt.plot(N, t_ref,
+#          label="assembly via gpu", color='y', linestyle='dotted', linewidth=10)
+# plt.plot(N, t_pipe,
+#          label="assembly via cpu", color='r')
+# plt.plot(N, t_cg,
+#          label="cg kernel", color='b')
+# #plt.plot(k, data , label = "time_custom")
+# plt.xscale('log', base=10)
+# plt.yscale('log', base=10)
+# plt.xlabel("grid size N (=points in one direction)")
+# plt.ylabel("computation time[ms]")
+# plt.legend()
+# plt.title(
+#     "Performance of GPU- vs CPU-powered matrix assembly on K40 compared to GPU-CG on RTX3060")
+# plt.grid()
+# plt.savefig("plots/ex2k40.jpg", bbox_inches='tight')
 
 
 

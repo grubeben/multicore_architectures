a2742570146dd51629f98448fd1ab9387fcd28da
grubeben <gr.game.98@gmail.com>
2022-12-08 21:45:32 +0100

    final commit

--- a8/plot.py
+++ a8/plot.py
@@ -25,7 +25,7 @@ data_5= np.genfromtxt(filepath5, dtype=float, delimiter=' ')
 
 filepath6 = "data/build_cash_rtx.txt"
 data_6= np.genfromtxt(filepath6, dtype=float, delimiter=' ')
-filepath7 = "data/build_cash_k40txt"
+filepath7 = "data/build_cash_k40.txt"
 data_7= np.genfromtxt(filepath7, dtype=float, delimiter=' ')
 
 
@@ -76,49 +76,80 @@ plt.grid()
 plt.savefig("plots/dotp.jpg", bbox_inches='tight')
 
 # plot - ex2 dotp
-N = []
-rtx_cuda= []
-rtx_ocl= []
-k40_cuda=[]
-k40_ocl=[]
-cpu=[]
+M = []
+rtx_build= []
+rtx_cache= []
+k40_build=[]
+k40_cache=[]
 
-for i in range(len(data_1)):
-    N.append(data_1
+for i in range(len(data_6)):
+    M.append(data_6
              [i][0])
-    rtx_cuda.append(data_3
+    rtx_build.append(data_6
              [i][1])
-    rtx_ocl.append(data_1
+    rtx_cache.append(data_6
+             [i][1]+data_6
+             [i][2])
+    k40_build.append(data_7
              [i][1])
-    k40_cuda.append(data_4
-             [i][1])
-    k40_ocl.append(data_2
-             [i][1])
-    cpu.append(data_5
-             [i][1])
+    k40_cache.append(data_7
+             [i][1]+data_7
+             [i][2])
+
+plt.figure(figsize=(10, 5))
+
+plt.plot(M, rtx_build,
+         label="RTX build time", color='y')
+plt.plot(M, rtx_cache,
+         label="RTX build + extract time", color='y', linestyle='dotted', linewidth=6)
+plt.plot(M, k40_build,
+         label="K40 build time", color='r')
+plt.plot(M, k40_cache,
+         label="K40 build + extract time", color='r',linestyle='dotted', linewidth=6)
+
+plt.xscale('log', base=10)
+plt.yscale('log', base=10)
+plt.xlabel("program length M")
+plt.ylabel("computation time[ms]")
+plt.legend()
+plt.title(
+    "OPENCL program build time (loading program into cache) in relation to extracting the kernels within (fetching from cache)")
+plt.grid()
+plt.savefig("plots/build_cache.jpg", bbox_inches='tight')
+
+# plot - ex2.1 
+M = []
+rtx_build= []
+rtx_cache= []
+k40_build=[]
+k40_cache=[]
+
+for i in range(len(data_6)):
+    M.append(data_6
+             [i][0])
+    rtx_build.append(data_6
+             [i][2])
+    k40_build.append(data_7
+             [i][2])
 
 
 plt.figure(figsize=(10, 5))
 
-plt.plot(N, rtx_cuda,
-         label="RTX CUDA", color='y')
-plt.plot(N, rtx_ocl,
-         label="RTX OCL", color='y', linestyle='dotted')
-plt.plot(N, k40_ocl,
-         label="K40 OCL", color='r',linestyle='dotted')
-plt.plot(N, k40_cuda,
-         label="K40 CUDA", color='r')
-plt.plot(N, cpu,
-         label="CPU (pthread-Intel(R) Core(TM) i7 CPU)", color='b')
+plt.plot(M, rtx_build,
+         label="RTX extract time", color='y')
+
+plt.plot(M, k40_build,
+         label="K40 extract time", color='r')
+
 
 plt.xscale('log', base=10)
 plt.yscale('log', base=10)
-plt.xlabel("vector size N")
+plt.xlabel("program length M")
 plt.ylabel("computation time[ms]")
 plt.legend()
 plt.title(
-    "Dot-Product Performance for OCL and CUDA implementations")
+    "extracting M kernels from a program in OPENCL")
 plt.grid()
-plt.savefig("plots/dotp.jpg", bbox_inches='tight')
+plt.savefig("plots/cache.jpg", bbox_inches='tight')
 
 
